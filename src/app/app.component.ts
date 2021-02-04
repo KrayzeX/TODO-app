47ff02851ab9f7caa637dcc44c7212b69305cebf
Aleksei Chistiakov <alexey.chisti@gmail.com>
2021-02-04 22:01:42 +0300

    add create and edit tasks functionality, fix ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,12 @@ export class AppComponent {
   completedTask: any;
   nCompleted: any;
   activeId: any;
+  postData: any = {};
+  updateData:any = {};
 
   constructor(
-    private http : HttpClient){}
+    private http : HttpClient
+    ){}
 
   ngOnInit(){
     this.http.get('https://jsonplaceholder.typicode.com/todos')
@@ -34,7 +37,7 @@ export class AppComponent {
                 return 1
               } else return -1;
             });
-            console.log(this.completedTask);
+            
             this.nCompleted = this.tasks.filter((element: any) => {
               if (!element.completed) {return true} else return false;
             }).sort(() => 0.5 - Math.random()).slice(0,10).sort((element1 : any, element2 :any) => {
@@ -45,8 +48,6 @@ export class AppComponent {
         } else alert('Something wrong');
     });
   }
-
-  createTask() {}
   
   toggle(id: number) {
     this.activeId = id;
@@ -62,12 +63,44 @@ export class AppComponent {
     });
   }
 
-  saveTask() {}
+  createTask(taskTitle: string, status: string) {
+    if (status == "To do"){
+      this.postData = {title: taskTitle, completed: false};
+      this.nCompleted.push(this.postData);
+    } else {
+      this.postData = {title: taskTitle, compiled: true};
+      this.completedTask.push(this.postData);
+    }
+    this.http.post('https://jsonplaceholder.typicode.com/todos', this.postData);
+
+  }
+
+  updateTask(taskTitle: string, status: string, id: number) {
+    if (status == "To do") {
+      this.updateData = {title: taskTitle, completed: false, id: id};
+      this.completedTask = this.completedTask.filter((item: any) => item.id !== id);
+      this.nCompleted = this.nCompleted.filter((item: any) => item.id !== id);
+      this.taskTitle = '';
+      this.task.completed = '';
+      this.nCompleted.push(this.updateData);
+    } else {
+      this.updateData = {title: taskTitle, completed: true, id: id};
+      this.completedTask = this.completedTask.filter((item: any) => item.id !== id);
+      this.nCompleted = this.nCompleted.filter((item: any) => item.id !== id);
+      this.taskTitle = '';
+      this.task.completed = ''; 
+      this.completedTask.push(this.updateData);
+    }
+    this.http.put("https://jsonplaceholder.typicode.com/todos/" + id, this.updateData)
+    .subscribe(data => console.log(data));
+  }
 
   deleteTask(id: number) {
     this.http.delete('https://jsonplaceholder.typicode.com/todos/' + id);
     this.completedTask = this.completedTask.filter((item: any) => item.id !== id);
+    this.nCompleted = this.nCompleted.filter((item: any) => item.id !== id);  
     this.taskTitle = '';
+    this.task.completed = '';
   }
 
 }
